d18e9bcd040c4a2777ce9a4a418e00985f567a42
Julia Muiruri <julia.muiruri4@gmail.com>
2023-10-17 11:44:18 +0300

    Initialize start

---
+++ Services/CosmosDbService.cs
@@ -0,0 +1,52 @@
+﻿using Cosmos.Chat.GPT.Models;
+
+namespace Cosmos.Chat.GPT.Services;
+
+public class CosmosDbService
+{
+    public CosmosDbService(string endpoint, string key, string databaseName, string containerName)
+    {
+        ArgumentNullException.ThrowIfNullOrEmpty(databaseName);
+        ArgumentNullException.ThrowIfNullOrEmpty(containerName);
+    }
+
+    public async Task<Session> InsertSessionAsync(Session session)
+    {
+        await Task.Delay(millisecondsDelay: 500);
+        return session;
+    }
+
+    public async Task<Message> InsertMessageAsync(Message message)
+    {
+        await Task.Delay(millisecondsDelay: 500);
+        return message;
+    }
+
+    public async Task<List<Session>> GetSessionsAsync()
+    {
+        await Task.Delay(millisecondsDelay: 500);
+        return Enumerable.Empty<Session>().ToList();
+    }
+
+    public async Task<List<Message>> GetSessionMessagesAsync(string sessionId)
+    {
+        await Task.Delay(millisecondsDelay: 500);
+        return Enumerable.Empty<Message>().ToList();
+    }
+
+    public async Task<Session> UpdateSessionAsync(Session session)
+    {
+        await Task.Delay(millisecondsDelay: 500);
+        return session;
+    }
+
+    public async Task UpsertSessionBatchAsync(params dynamic[] messages)
+    {
+        await Task.Delay(millisecondsDelay: 500);
+    }
+
+    public async Task DeleteSessionAndMessagesAsync(string sessionId)
+    {
+        await Task.Delay(millisecondsDelay: 500);
+    }
+}
---
+++ Services/OpenAiService.cs
@@ -0,0 +1,25 @@
+﻿using Cosmos.Chat.GPT.Models;
+
+namespace Cosmos.Chat.GPT.Services;
+
+public class OpenAiService
+{
+    private readonly string _modelName = String.Empty;
+
+    public OpenAiService(string endpoint, string key, string modelName)
+    {
+        ArgumentNullException.ThrowIfNullOrEmpty(modelName);
+    }
+
+    public async Task<(string response, int promptTokens, int responseTokens)> GetChatCompletionAsync(string sessionId, string userPrompt)
+    {
+        await Task.Delay(millisecondsDelay: 500);
+        return ("&ltRESPONSE&gt;", 0, 0);
+    }
+
+    public async Task<string> SummarizeAsync(string sessionId, string userPrompt)
+    {
+        await Task.Delay(millisecondsDelay: 500);
+        return "&ltSUMMARY&gt;";
+    }
+}
